613d2f3d930f3f292b2b4d230ef45320e89300d1
hasnaindev23 <hasnainop525@gmail.com>
2024-03-01 18:57:00 -0700

    nit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
+const getUser = document.querySelector('#user') as HTMLInputElement;
+const formSubmit = document.querySelector('.form') as HTMLFormElement;
+const main_container = document.querySelector('.main-container') as HTMLDivElement;
 
+//so lets define the contract of an object
+
+interface UserData {
+    id: number;
+    login: string;
+    avatar_url: string;
+    location: string;
+    url: string;
+}
